08f325b09dfda4037cbedb36ac23552c77d3e119
Alexssh <alehakrut@gmail.com>
2025-08-31 19:21:40 +0700

    git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header"
+import Main from "./pages/Main/Main"
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <div className="container">
+        <Main></Main>
+      </div>
+    </>
+  )
+}
+
+export default App
